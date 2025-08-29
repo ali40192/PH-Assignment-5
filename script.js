e3169de1306389c40ff1bc7498bd04697fa76bf3
Mohammad Ali <mohammadali40192@gmail.com>
2025-08-29 15:52:57 +0600

    added a condition

--- script.js
+++ script.js
@@ -32,9 +32,28 @@ const heartBtns = document.getElementsByClassName("heart");
           let title =callBtn.parentNode.parentNode.children[1].children[1].innerText;
           let serviceNum = callBtn.parentNode.parentNode.children[2].children[0].innerText;
         
-        let text =  alert('Calling'+' '+ title +' '+ serviceNum + '...');
+          
+             let coin =gerElement('coin-number').innerText;
+          let numOfcoin =parseInt(coin);
+         let finalCoin = numOfcoin -20;
+         gerElement('coin-number').innerText = finalCoin;
+         if ( finalCoin > 20) {
+            alert('Calling'+' '+ title +' '+ serviceNum + '...');
+         }
+        
+
+        else if (finalCoin < 20) {
+          alert("You don't have sufficient coin. you need at least 20 coins to make a call");
+          return gerElement('coin-number').innerText = 0;
+         }
          
-        };
+      
+          }
+        
+        
+        
+        
+        
         
     })
 
