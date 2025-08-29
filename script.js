f3f255ef35211da74e692efa7550a7f82418e669
Mohammad Ali <mohammadali40192@gmail.com>
2025-08-29 18:06:38 +0600

    added history sec

--- script.js
+++ script.js
@@ -31,7 +31,23 @@ const heartBtns = document.getElementsByClassName("heart");
           const callBtn =e.target;
           let title =callBtn.parentNode.parentNode.children[1].children[1].innerText;
           let serviceNum = callBtn.parentNode.parentNode.children[2].children[0].innerText;
-        
+          let hisfirstElement = callBtn.parentNode.parentNode.children[1].children[0].innerText;
+
+          let date =new Date().toLocaleTimeString();
+
+          let makeHistory = gerElement('add-hisbtn');
+          let makeNewhis = document.createElement('div');
+          makeNewhis.innerHTML = `
+          
+          <div class="rounded-md shadow-sm p-4 my-4 flex justify-between items-center">
+          <div><h1 class="text-sm font-bold">${hisfirstElement}</h1>
+          <h1 class="text-sm">${serviceNum}</h1></div>
+          <div>${date}</div>
+          </div>
+          
+          `
+          makeHistory.appendChild(makeNewhis);
+          
           
              let coin =gerElement('coin-number').innerText;
           let numOfcoin =parseInt(coin);
@@ -39,23 +55,28 @@ const heartBtns = document.getElementsByClassName("heart");
          gerElement('coin-number').innerText = finalCoin;
          if ( finalCoin > 20) {
             alert('Calling'+' '+ title +' '+ serviceNum + '...');
+
+
+
+
          }
         
 
         else if (finalCoin < 20) {
           alert("You don't have sufficient coin. you need at least 20 coins to make a call");
-          return gerElement('coin-number').innerText = 0;
+           gerElement('coin-number').innerText = 0;
+          makeNewhis.innerHTML =' ';
          }
          
       
           }
-        
-        
-        
-        
-        
-        
-    })
+
+
+          gerElement('history-btn').addEventListener('click', function(){
+            gerElement('add-hisbtn').innerHTML = ' ';
+          })
+           
+    });
 
 
 
